9e7933a4c2829c6e6b9f5476b1eb732f1bcb657f
anderojr <and.okuda@gmail.com>
2023-05-12 02:57:34 -0300

    rubocop rules

--- caesar_cypher.rb
+++ caesar_cypher.rb
@@ -1,28 +1,29 @@
+# frozen_string_literal: true
+
 def caesar_cipher(string, shift_factor, operation)
-    result = ''
-    string.each_char do |char|
-      ascii_char = char.ord
-      if char.between?('A', 'Z') || char.between?('a', 'z')
-        if operation == 1 #encrypt
-          new_ascii = ascii_char + shift_factor
-          if(ascii_char <= 'Z'.ord)
-              new_ascii -= 26 if new_ascii > 'Z'.ord
-          else
-              new_ascii -= 26 if new_ascii > 'z'.ord
-          end
-          result += new_ascii.chr
-        else #decrypt
-          new_ascii = ascii_char - shift_factor
-          if(ascii_char <= 'Z'.ord)
-              new_ascii += 26 if new_ascii < 'A'.ord
-          else
-              new_ascii += 26 if new_ascii < 'a'.ord
-          end
-          result += new_ascii.chr
-      end    
-      else
-        result += char
+  result = ''
+  string.each_char do |char|
+    ascii_char = char.ord
+    if char.between?('A', 'Z') || char.between?('a', 'z')
+      if operation == 1 # encrypt
+        new_ascii = ascii_char + shift_factor
+        if ascii_char <= 'Z'.ord
+          new_ascii -= 26 if new_ascii > 'Z'.ord
+        elsif new_ascii > 'z'.ord
+          new_ascii -= 26
+        end
+      else # decrypt
+        new_ascii = ascii_char - shift_factor
+        if ascii_char <= 'Z'.ord
+          new_ascii += 26 if new_ascii < 'A'.ord
+        elsif new_ascii < 'a'.ord
+          new_ascii += 26
+        end
       end
+      result += new_ascii.chr
+    else
+      result += char
     end
-    result
   end
+  result
+end
